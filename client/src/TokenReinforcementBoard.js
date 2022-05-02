db221d54d028a69af84931a4b47bd3a6f5e23326
Tunisia Artope <artope@yahoo.com>
2022-05-02 10:29:28 -0400

    changed css from px to percentage to improve responsiveness

--- client/src/TokenReinforcementBoard.js
+++ client/src/TokenReinforcementBoard.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import TokenReinforcementCard from "./TokenReinforcementCard";
+import { BsStarFill, BsStar } from "react-icons/bs";
 import './TokenReinforcementBoard.css'; 
 
 function TokenReinforcementBoard({ tokenVisualCards, handleRemoveFromTokenReinforcementBoard, handleDeleteVisualCard }){
-    console.log(!!tokenVisualCards.length);
+    // console.log(!!tokenVisualCards.length);
+
+    const [emptyStar, setEmptyStar] = useState(null)
+    const [filledStar, setFilledStar] = useState(true)
+
     const visCard = tokenVisualCards.map(tokenVisualCard => {
         // console.log(tokenVisualCard) 
         return <TokenReinforcementCard key={tokenVisualCard.id} visualCard={tokenVisualCard} onClickedCard={handleRemoveFromTokenReinforcementBoard} onClickDelete={handleDeleteVisualCard} />
 
     })
+
+    function toggleStar(){
+        if(emptyStar === true){
+        setEmptyStar(emptyStar)
+        }else{
+            
+        }
+    }
 
     return(
         <div className="token-reinforcement-board-container">
@@ -22,8 +35,13 @@ function TokenReinforcementBoard({ tokenVisualCards, handleRemoveFromTokenReinfo
             {/* <div className="token-reinforcement-board-visual-cards-container" >
                 { visCard }
             </div> */}
+            <div className="star">
+                <BsStar size={120} onClick={()=>console.log('click1')}/> <BsStar size={120} onClick={()=>console.log('click2')}/> <BsStar size={120} onClick={()=>console.log('click3')}/> <BsStar size={120} onClick={()=>console.log('click4')}/> <BsStar size={120} onClick={()=>console.log('click5')}/>
+            </div>
         </div>
     )
 }
 
 export default TokenReinforcementBoard;
+
+// per console, '!!tokenVisualCards.length' is false. so ternary: if false put 'visCard', else put blank div.
