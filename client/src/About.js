a6ac0927804b7181df09b6fd4b5363ea5712c859
Tunisia Artope <artope@yahoo.com>
2022-05-11 16:05:54 -0400

    demo video

--- client/src/About.js
+++ client/src/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import ReactPlayer from "react-player";
 import './About.css';
 
 function About(){
 
     return(
         <div className="about">
-            <h2>
-                A convenient way to access and use communication visuals for special education staff who work with students on the autism spectrum. Access communication visuals in the app. Click on a communication visual to add it to your First/Then Board or Token Reinforcement Board the same way you would with your physical communication boards. Create the communication visuals that you need and delete the ones you don't need.
-            </h2>
-            
+            <div className="about-summary">
+                <h2>
+                    A convenient way to access and use communication visuals for special education staff who work with students on the autism spectrum. Click on a communication visual to add it to your First/Then Board or Token Reinforcement Board. Create the communication visuals that you need and delete the ones you don't need.
+                </h2>
+            </div>
+            <div className="about-demo">
+                <ReactPlayer controls url="https://youtu.be/sVWZ96P96tc"/>
+            </div>
         </div>
     )
 }
